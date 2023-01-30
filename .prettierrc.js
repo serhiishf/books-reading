20fb10afaf8bcb9fdc36af50630dd076af59ac16
serhiishf <unison447@gmail.com>
2023-01-30 15:35:13 +0200

    init: Add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'all',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  printWidth: 80,
+  // Rules for React
+  jsxBracketSameLine: false,
+};
