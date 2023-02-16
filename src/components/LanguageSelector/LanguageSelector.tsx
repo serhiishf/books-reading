2161b6961d6b238e3f925f9e8092e0a181e467fe
Katerina Zamiatina <eozamyatina@gmail.com>
2023-02-16 13:51:51 +0100

    fix: logout

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -4,8 +4,8 @@ import { useTranslation } from 'react-i18next';
 export const LanguageSelector = () => {
   const { i18n } = useTranslation();
 
-//   const changeLanguage = (lang: string) => {
-//     i18n.changeLanguage(lang);
-//     localStorage.setItem('LOCALE', lang);
-//   };
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    localStorage.setItem('LOCALE', lang);
+  };
 };
