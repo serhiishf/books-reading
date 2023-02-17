9c400ffd7fcdc6895206aacf9b55030769184c65
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-02-17 23:06:41 +0100

    feat: add i18n to page 404 and fix public manifest file

--- src/views/Page404/Page404.tsx
+++ src/views/Page404/Page404.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import { useAppSelector } from '../../redux/app/hooks';
@@ -6,16 +7,21 @@ import authSelectors from '../../redux/features/auth/authSelectors';
 import styles from './Page404.module.scss';
 
 export default function Page404() {
+  const { t } = useTranslation();
   const isLogged = useAppSelector(authSelectors.getLoggedOn);
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>404</h1>
-      <p className={styles.txt1}>Article not found</p>
-      <p className={styles.txt2}>The page you requested cannot be found.</p>
+      <p className={styles.txt1}>{t('articleNotFound')}</p>
+      <p className={styles.txt2}>{t('pageCanNotBeFound')}</p>
       <Link className={styles.link} to={isLogged ? '/library' : '/login'}>
-        Return to the Homepage
+        {t('returnToHomepage')}
       </Link>
     </div>
   );
 }
+
+//Article not found - 'articleNotFound'
+//The page you requested cannot be found. - 'pageCanNotBeFound'
+//Return to the Homepage = 'returnToHomepage'
