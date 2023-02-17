e3e521966537bb0a7a9b27ed4b2358456e3083ba
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-02-17 01:12:38 +0100

    feat: add page 404

--- src/views/routes.ts
+++ src/views/routes.ts
@@ -6,8 +6,6 @@ const LoginPage = lazy(() => import('./LoginPage'));
 
 const RegisterPage = lazy(() => import('./RegisterPage'));
 
-const AboutPage = lazy(() => import('./AboutPage'));
-
 const LibraryPage = lazy(() => import('./LibraryPage'));
 
 const TrainingPage = lazy(() => import('./TrainingPage'));
@@ -48,14 +46,7 @@ const routes = [
     isNav: true,
     redirectTo: '/library',
   },
-  {
-    path: '/about',
-    label: 'About us',
-    component: AboutPage,
-    isProtected: false,
-    isNav: true,
-    redirectTo: '/about',
-  },
+
   {
     path: '/library',
     component: LibraryPage,
@@ -73,7 +64,7 @@ const routes = [
     redirectTo: '/training',
   },
   {
-    path: '',
+    path: '/*',
     component: Page404,
     label: '',
     isProtected: false,
