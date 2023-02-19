be57bdff96cc140aa60761411ae316d67e8583e0
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-02-19 21:25:08 +0100

    feat: add i18n for quotes, refactor LoginPage

--- src/services/getRandomNum.ts
+++ src/services/getRandomNum.ts
@@ -1,5 +1,5 @@
-export default function getRandomNum() {
-  const min = Math.ceil(0);
-  const max = Math.floor(11);
+export default function getRandomNum(start: number, end: number) {
+  const min = Math.ceil(start);
+  const max = Math.floor(end);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
