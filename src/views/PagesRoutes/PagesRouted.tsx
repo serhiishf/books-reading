1116701315a12aac963a67e4ff9f8594a2aa6285
Katerina Zamiatina <eozamyatina@gmail.com>
2023-02-20 14:26:31 +0100

    fix: url path after login&navigation in user account

--- src/views/PagesRoutes/PagesRouted.tsx
+++ src/views/PagesRoutes/PagesRouted.tsx
@@ -19,9 +19,9 @@ const PagesRoutes = () => {
       <Header />
       <Suspense fallback={<Loader />}>
         <Routes>
+          <Route path="/" element={<IntroPage />} />
           <Route element={<PublicRoute />}>
-            <Route path="/" element={<IntroPage />} />
-            <Route path="/login" element={<LoginPage />}/>
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
           </Route>
           <Route element={<PrivateRoute />}>
