c23aa8720075000652dcddbc341c4eacce4cb080
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-02-21 21:27:27 +0100

    feat: add burger menu

---
+++ src/components/NavAuth/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import styles from './MobileMenu.module.scss';
+import Hamburger from '../../Hamburger';
+interface Props {
+  logoutClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const MobileMenu: React.FC<Props> = ({ logoutClick }) => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
+  return (
+    <>
+      <div className={styles.contentBox}>
+        {hamburgerOpen && (
+          <>
+            <div className={styles.list}>
+              <div>link</div>
+              <div>link</div>
+              <button type="button" onClick={logoutClick}>
+                logout
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+
+      <button
+        type="button"
+        onClick={toggleHamburger}
+        className={styles.burgerBtn}
+      >
+        <Hamburger isOpen={hamburgerOpen} />
+      </button>
+    </>
+  );
+};
+
+export default MobileMenu;
--- src/components/NavAuth/NavAuth.tsx
+++ src/components/NavAuth/NavAuth.tsx
@@ -1,20 +1,19 @@
-import { ReactComponent as SvgLibrary } from '../../assets/img/book.svg';
-import { ReactComponent as SvgHome } from '../../assets/img/home.svg';
-import { NavLink } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
 import React from 'react';
-import styles from './NavAuth.module.scss';
+
 import { useAppDispatch, useAppSelector } from '../../redux/app/hooks';
 import authOperations from '../../redux/features/auth/authOperations';
 import authSelectors from '../../redux/features/auth/authSelectors';
-import { useTranslation } from 'react-i18next';
-import UserName from '../UserName';
 import useViewportSizes from '../../hooks/useViewportSizes';
 import breakpoints from '../../utils/breakpoints';
 
+import UserName from '../UserName';
+import DesktopMenu from './DesktopMenu';
+import MobileMenu from './MobileMenu';
+
+import styles from './NavAuth.module.scss';
+
 export default function NavAuth() {
   const dispatch = useAppDispatch();
-  const { t } = useTranslation();
 
   const userName = useAppSelector(authSelectors.getUserName);
   const sizes = useViewportSizes();
@@ -23,9 +22,7 @@ export default function NavAuth() {
   const onLogoutClick = async () => {
     await dispatch(authOperations.logOut());
   };
-  //algoritm:
-  //create 2 components: burgerMenu and desktopMenu
-  //onLogoutClick in props to both
+
   //styled burger menu
   //add portal and modal for asking about logout
 
@@ -33,26 +30,9 @@ export default function NavAuth() {
     <div className={styles.navWrapper}>
       <UserName userNameStr={userName} />
 
-      {isMobileView && <div>burger</div>}
+      {isMobileView && <MobileMenu logoutClick={onLogoutClick} />}
 
-      {!isMobileView && (
-        <div className={styles.linksWrapper}>
-          <NavLink className={styles.navLink} key={uuidv4()} to={'/library'}>
-            <SvgLibrary className={styles.icon} />
-          </NavLink>
-          <NavLink className={styles.navLink} key={uuidv4()} to={'/training'}>
-            <SvgHome className={styles.icon} />
-          </NavLink>
-          <NavLink
-            className={styles.navLink}
-            key={uuidv4()}
-            to={'/'}
-            onClick={onLogoutClick}
-          >
-            {t('auth.logout')}
-          </NavLink>
-        </div>
-      )}
+      {!isMobileView && <DesktopMenu logoutClick={onLogoutClick} />}
     </div>
   );
 }
