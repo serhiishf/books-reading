4654f51ec245a7207e470225bd4c48574e024eb5
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-02-27 10:31:38 +0100

    fix: return prev logic to training page

--- src/views/TrainingPage/TrainingPage.tsx
+++ src/views/TrainingPage/TrainingPage.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import styles from './TrainingPage.module.scss';
 import CreateTraining from '../../components/TrainingPages/CreateTrainingPage';
-import TrainingDiagram from '../../components/TrainingDiagram';
 
 export default function TrainingPage() {
   return (
     <div className={styles.mainWrapper}>
       <div className={styles.containerWrap}>
         <CreateTraining />
-
-        <div className={styles.bottomPosition}>
-          <TrainingDiagram />
-          {/* statistic component */}
-        </div>
       </div>
     </div>
   );
 }
+
+// <div className={styles.bottomPosition}>
+//   <TrainingDiagram />
+//   {/* statistic component */}
+// </div>
