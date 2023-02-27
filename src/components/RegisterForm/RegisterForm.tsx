d7c1069083de13f2f9630608465c2df77d1d2fe8
Katerina Zamiatina <eozamyatina@gmail.com>
2023-02-27 21:43:07 +0100

    fix: delete console.log

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -58,7 +58,6 @@ export default function RegisterForm() {
     }),
     onSubmit: async (values) => {
       const { name, email, password } = values;
-      console.log(email);
       dispatch(authOperations.register({ name, email, password })).then(
         (res: RegRes) => {
           if (res) {
