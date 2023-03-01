009f83ff8e0ab52a67102df96f111bb9eb4b9cae
Marina Tripetska <marina.tripetskaya@gmail.com>
2023-03-01 18:38:29 +0100

    fix: logic render diagram on add stats

--- src/components/Training/Diagram/generateData.ts
+++ src/components/Training/Diagram/generateData.ts
@@ -2,11 +2,6 @@ import { Book } from '../../../services/books/books-service';
 import { ReadingTraining } from '../../../services/training/training-service';
 import { Datum, IData } from './types';
 
-// interface Datum {
-//   x: string;
-//   y: number;
-// }
-
 function formatDate(dateStr: string): string {
   const date = new Date(dateStr);
   const day = date.getDate().toString().padStart(2, '0');
@@ -60,20 +55,64 @@ function generatePlan(training: ReadingTraining): Datum[] {
   return planData;
 }
 
+// function generateFact(training: ReadingTraining): Datum[] {
+//   const { start, finish, statistics, totalPages } = training;
+
+//   if (statistics.length === 0) {
+//     const average = getAveragePagesPerDay(start, finish, totalPages);
+//     const factData: Datum[] = [{ x: formatDate(start), y: average + 5 }];
+//     return factData;
+//   } else {
+//     const factData: Datum[] = [];
+//     statistics.forEach(({ date, pages }) => {
+//       factData.push({ x: formatDate(date), y: pages });
+//     });
+//     return factData;
+//   }
+// }
 function generateFact(training: ReadingTraining): Datum[] {
   const { start, finish, statistics, totalPages } = training;
+  const factData: Datum[] = [];
+  const startObj = new Date(start);
+  const finishObj = new Date(finish);
 
   if (statistics.length === 0) {
     const average = getAveragePagesPerDay(start, finish, totalPages);
-    const factData: Datum[] = [{ x: formatDate(start), y: average + 5 }];
-    return factData;
-  } else {
-    const factData: Datum[] = [];
-    statistics.forEach(({ date, pages }) => {
-      factData.push({ x: formatDate(date), y: pages });
-    });
+    factData.push({ x: formatDate(start), y: average + 5 });
     return factData;
   }
+
+  let prevDate = new Date(start);
+  let sumPages = 0;
+
+  for (let i = 0; i < statistics.length; i++) {
+    const statDate = new Date(statistics[i].date);
+
+    if (statDate >= prevDate && statDate <= finishObj) {
+      sumPages += statistics[i].pages;
+      prevDate = statDate;
+    } else {
+      if (sumPages > 0) {
+        const formattedDate = formatDate(prevDate.toISOString());
+        factData.push({ x: formattedDate, y: sumPages });
+        sumPages = 0;
+      }
+      if (statDate >= startObj && statDate <= finishObj) {
+        sumPages += statistics[i].pages;
+        prevDate = statDate;
+      }
+    }
+  }
+
+  if (sumPages > 0) {
+    const formattedDate = formatDate(prevDate.toISOString());
+    factData.push({ x: formattedDate, y: sumPages });
+  } else if (factData.length === 0) {
+    const average = getAveragePagesPerDay(start, finish, totalPages);
+    factData.push({ x: formatDate(start), y: average + 5 });
+  }
+
+  return factData;
 }
 
 export function generateData(training: ReadingTraining): IData[] {
