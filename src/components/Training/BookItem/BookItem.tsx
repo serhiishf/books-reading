445bac810d6fa5703734069464b883f24a9009f3
Serhii Sh <unison447@gmail.com>
2023-03-01 22:18:46 +0200

    feat: Add styles for list books in training

--- src/components/Training/BookItem/BookItem.tsx
+++ src/components/Training/BookItem/BookItem.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import styles from './BookItem.module.scss';
 import { Book } from '../../../services/books/books-service';
+import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
 
 interface Props {
   book: Book;
 }
 
 const BookItem: React.FC<Props> = ({ book }) => {
+  const { t } = useTranslation();
   return (
     <div className={styles.thumb} key={book._id}>
-      <p className={styles.name}>{book.name}</p>
-      <p className={styles.author}>{book.author}</p>
-      <p className={styles.year}>{book.year}</p>
-      <p className={styles.pages}>{book.pages}</p>
+      <div className={styles.wrapTitles}>
+        <div className={classNames(styles.wrapItem, styles.textItem)}>
+          <div className={styles.subtitle}></div>
+          <p className={(styles.name)}>{book.name}</p>
+        </div>
+        <div className={classNames(styles.wrapItem, styles.textItem)}>
+          <div className={styles.subtitle}>{`${t('training.author')}:`}</div>
+          <p className={(styles.author)}>{book.author}</p>
+        </div>
+        <div className={classNames(styles.wrapItem, styles.numberItem)}>
+          <div className={styles.subtitle}>{`${t('training.year')}:`}</div>
+          <p className={(styles.year)}>{book.year}</p>
+        </div>
+        <div className={classNames(styles.wrapItem, styles.numberItem)}>
+          <div className={styles.subtitle}>{`${t('training.pages')}:`}</div>
+          <p className={(styles.pages)}>{book.pages}</p>
+        </div>
+      </div>
+      <p className={(styles.delete, styles.iconItem)}>
+        {/* TODO: INSERT DELETE element here */}
+      </p>
     </div>
   );
 };
