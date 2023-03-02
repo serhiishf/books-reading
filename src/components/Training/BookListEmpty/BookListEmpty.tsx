8855761f07ee51623faf1a73aeaf42435719a790
Katerina Zamiatina <eozamyatina@gmail.com>
2023-03-02 13:44:35 +0100

    feat: add styles to book list empty

--- src/components/Training/BookListEmpty/BookListEmpty.tsx
+++ src/components/Training/BookListEmpty/BookListEmpty.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import BookListHeader from '../BookListHeader';
 import { Book } from '../../../services/books/books-service';
 import { ReactComponent as BookImg } from '../../../assets/img/book.svg';
+import { AiOutlineDelete } from 'react-icons/ai';
 import BookItem from '../BookItem';
 import styles from './BookListEmpty.module.scss';
 
@@ -10,6 +11,10 @@ type Props = {
 };
 
 const BookListEmpty: FC<Props> = ({ activeBooks }) => {
+  const onOpenClick = () => {
+    console.log('!!!');
+  };
+
   return (
     <div>
       <BookListHeader />
@@ -17,8 +22,13 @@ const BookListEmpty: FC<Props> = ({ activeBooks }) => {
         {activeBooks.map((book) => {
           return (
             <li className={styles.bookName} key={book._id}>
-              <BookImg className={styles.icon} />
-              <BookItem key={book._id} book={book} />
+              <BookImg className={styles.iconBook} />
+              <div className={styles.bookItem}>
+                <BookItem key={book._id} book={book} />
+              </div>
+              <button className={styles.btnDel} onClick={onOpenClick}>
+                <AiOutlineDelete color="#a6abb9" style={{ fontSize: '20px' }} />
+              </button>
             </li>
           );
         })}
